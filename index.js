ae3bcb668c9bf642a8718b162b800df84b1aff77
Gabriel Matos <g.matos@outlook.com>
2021-09-29 19:29:11 -0300

    finalizei a land page conforme aula 06

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000; // Const para armanezar a porta do servidor
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 // Substituição de function por arrow function
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/index", (req, res) => {
+  res.render("index", { titulo: "Blue" });
 });
 
 // Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
